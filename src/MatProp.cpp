f415917c3b4358573afb73bdba8456d5310766f8
Shashank Subramanian <shashank1693@gmail.com>
2017-10-04 13:08:14 -0500

    Matprop changed to not read data always

--- src/MatProp.cpp
+++ src/MatProp.cpp
@@ -22,36 +22,50 @@ PetscErrorCode MatProp::setValues (std::shared_ptr<NMisc> n_misc) {
 	PetscFunctionBegin;
 	PetscErrorCode ierr;
 	double *gm_ptr, *wm_ptr, *csf_ptr, *glm_ptr, *filter_ptr;
+	int procid, nprocs;
+    MPI_Comm_size (MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank (MPI_COMM_WORLD, &procid);
+
 	if (n_misc->testcase_ != BRAIN) {
 		ierr = VecSet (filter_, 1.0);						  CHKERRQ (ierr);
 	}
 	else {
-		ierr = VecGetArray (gm_, &gm_ptr);                    CHKERRQ (ierr);
-		ierr = VecGetArray (wm_, &wm_ptr);                    CHKERRQ (ierr);
-		ierr = VecGetArray (csf_, &csf_ptr);                  CHKERRQ (ierr);
-		ierr = VecGetArray (glm_, &glm_ptr);                  CHKERRQ (ierr);
-		ierr = VecGetArray (filter_, &filter_ptr);            CHKERRQ (ierr);
-
-
-		dataIn (gm_ptr, n_misc, "gray_matter.nc");
-		dataIn (wm_ptr, n_misc, "white_matter.nc");
-		dataIn (csf_ptr, n_misc, "csf.nc");
-		dataIn (glm_ptr, n_misc, "glial_matter.nc");
-		dataIn (filter_ptr, n_misc, "filter_zero.nc");
-
-		if(n_misc->writeOutput_) {
-			dataOut (gm_ptr, n_misc, "results/gray_matter.nc");
-			dataOut (wm_ptr, n_misc, "results/white_matter.nc");
-			dataOut (csf_ptr, n_misc, "results/csf.nc");
-			dataOut (glm_ptr, n_misc, "results/glial_matter.nc");
-			dataOut (filter_ptr, n_misc, "results/filter_zero.nc");
+		std::stringstream str;
+		const char *prefix = "./brain_data/";
+		str << prefix << "/" << n_misc->n_[0] << "/gray_matter.nc";
+		std::ifstream data_file (str.str().c_str());
+		if (!data_file.is_open()) {
+			//Do nothing
+			PCOUT << "---Brain default data not read: Expecting user data ----" << std::endl;
 		}
+		else {
+			ierr = VecGetArray (gm_, &gm_ptr);                    CHKERRQ (ierr);
+			ierr = VecGetArray (wm_, &wm_ptr);                    CHKERRQ (ierr);
+			ierr = VecGetArray (csf_, &csf_ptr);                  CHKERRQ (ierr);
+			ierr = VecGetArray (glm_, &glm_ptr);                  CHKERRQ (ierr);
+			ierr = VecGetArray (filter_, &filter_ptr);            CHKERRQ (ierr);
 
-		ierr = VecRestoreArray (gm_, &gm_ptr);                    CHKERRQ (ierr);
-		ierr = VecRestoreArray (wm_, &wm_ptr);                    CHKERRQ (ierr);
-		ierr = VecRestoreArray (csf_, &csf_ptr);                  CHKERRQ (ierr);
-		ierr = VecRestoreArray (glm_, &glm_ptr);                  CHKERRQ (ierr);
-		ierr = VecRestoreArray (filter_, &filter_ptr);            CHKERRQ (ierr);
+
+			dataIn (gm_ptr, n_misc, "gray_matter.nc");
+			dataIn (wm_ptr, n_misc, "white_matter.nc");
+			dataIn (csf_ptr, n_misc, "csf.nc");
+			dataIn (glm_ptr, n_misc, "glial_matter.nc");
+			dataIn (filter_ptr, n_misc, "filter_zero.nc");
+
+			if(n_misc->writeOutput_) {
+				dataOut (gm_ptr, n_misc, "results/gray_matter.nc");
+				dataOut (wm_ptr, n_misc, "results/white_matter.nc");
+				dataOut (csf_ptr, n_misc, "results/csf.nc");
+				dataOut (glm_ptr, n_misc, "results/glial_matter.nc");
+				dataOut (filter_ptr, n_misc, "results/filter_zero.nc");
+			}
+
+			ierr = VecRestoreArray (gm_, &gm_ptr);                    CHKERRQ (ierr);
+			ierr = VecRestoreArray (wm_, &wm_ptr);                    CHKERRQ (ierr);
+			ierr = VecRestoreArray (csf_, &csf_ptr);                  CHKERRQ (ierr);
+			ierr = VecRestoreArray (glm_, &glm_ptr);                  CHKERRQ (ierr);
+			ierr = VecRestoreArray (filter_, &filter_ptr);            CHKERRQ (ierr);
+		}
 	}
 
 	PetscFunctionReturn(0);
