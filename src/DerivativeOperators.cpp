d75e9bb1945b0eccf87d790e56a41617fbf0d13b
Klaudius Scheufele <klaudius.scheufele@ipvs.uni-stuttgart.de>
2017-11-30 19:18:03 -0600

    removed comments

--- src/DerivativeOperators.cpp
+++ src/DerivativeOperators.cpp
@@ -310,7 +310,7 @@ PetscErrorCode DerivativeOperatorsPos::evaluateHessian (Vec y, Vec x) {
 
 /* #### ------------------------------------------------------------------- #### */
 /* #### ========    REACTION DIFFUSION W/ MODIFIED OBJECTIVE (MP)  ======== #### */
-/* #### ========    REACTION DIFFUSION FOR MOVING ATLAS (MA)      ======== #### */
+/* #### ========    REACTION DIFFUSION FOR MOVING ATLAS (MA)       ======== #### */
 /* #### ------------------------------------------------------------------- #### */
 PetscErrorCode DerivativeOperatorsRDObj::evaluateObjective (PetscReal *J, Vec x, Vec data) {
     PetscFunctionBegin;
@@ -446,103 +446,6 @@ PetscErrorCode DerivativeOperatorsRDObj::evaluateHessian (Vec y, Vec x){
 
     PetscFunctionReturn(0);
 }
-
-
-// /* #### ------------------------------------------------------------------- #### */
-// /* #### ========    REACTION DIFFUSION FOR MOVING ATLAS (MA)       ======== #### */
-// /* #### ------------------------------------------------------------------- #### */
-// PetscErrorCode DerivativeOperatorsRDMovingAtlas::evaluateObjective (PetscReal *J, Vec x, Vec data) {
-//     PetscFunctionBegin;
-//     PetscErrorCode ierr = 0;
-//     TU_assert (data != nullptr, "DerivativeOperatorsRDMovingAtlas::evaluateObjective: requires non-null input data.");
-//     PetscScalar misfit_tu = 0, misfit_brain = 0;
-//     PetscReal reg;
-//     n_misc_->statistics_.nb_obj_evals++;
-//
-//     //compute c0
-//     ierr = tumor_->phi_->apply (tumor_->c_0_, x);                CHKERRQ (ierr);
-//     // compute c1
-//     ierr = pde_operators_->solveState (0);                       CHKERRQ (ierr);
-//     ierr = tumor_->obs_->apply (temp_, tumor_->c_t_);            CHKERRQ (ierr);
-//     // compute m_tilde = mA(1)(1-c(1))
-//     ierr = geometricCoupling(
-//       tumor_->work_[7], tumor_->work_[8], tumor_->work_[9], tumor_->work_[10], tumor_->work_[11],
-//       mA1_wm_, mA1_gm_, mA1_csf_, mA1_glm_,  mA1_bg_,
-//       tumor_->c_t_, n_misc_);                                    CHKERRQ (ierr);
-//     // evaluate tumor distance meassure || c(1) - d ||
-//     ierr = VecAXPY (temp_, -1.0, data);                          CHKERRQ (ierr);
-//     ierr = VecDot (temp_, temp_, &misfit_tu);                    CHKERRQ (ierr);
-//     // evaluate brain tissue distance meassure || mT - mR ||, mT = mA(1)(1-c(1)), mR = patient
-//     computeDifference(&misfit_brain,
-//       xi_wm_, xi_gm_, xi_csf_, xi_glm_,  xi_bg_,
-//       tumor_->work_[7], tumor_->work_[8], tumor_->work_[9], tumor_->work_[10], tumor_->work_[11],
-//       mR_wm_, mR_gm_, mR_csf_, mR_glm_,  mR_bg_);                CHKERRQ (ierr);
-//     // compute regularization
-//     ierr = VecDot (tumor_->c_0_, tumor_->c_0_, &reg);            CHKERRQ (ierr);
-//     reg *= 0.5 * n_misc_->beta_;
-//     // compute objective function value
-//     misfit_brain *= 0.5;
-//     misfit_tu  *= 0.5;
-//     (*J) = misfit_tu + misfit_brain;
-//     (*J) *= 1./nc_;
-//     (*J) += reg;
-//
-//     std::stringstream s;
-//     s << "  J(v) = Dm(v,c) + Dc(c) + S(c0) = "<< std::setprecision(12) << (*J) <<" = " << std::setprecision(12) <<misfit_brain * 1./nc_ <<" + "<< std::setprecision(12)<< misfit_tu * 1./nc_ <<" + "<< std::setprecision(12) <<reg<<"";  ierr = tuMSGstd(s.str()); CHKERRQ(ierr); s.str(""); s.clear();
-//     PetscFunctionReturn(0);
-// }
-//
-// PetscErrorCode DerivativeOperatorsRDMovingAtlas::evaluateGradient (Vec dJ, Vec x, Vec data) {
-//     PetscFunctionBegin;
-//     PetscErrorCode ierr = 0;
-//     PetscScalar misfit_brain;
-//     n_misc_->statistics_.nb_grad_evals++;
-//
-//     ierr = tumor_->phi_->apply (tumor_->c_0_, x);                CHKERRQ (ierr);
-//     ierr = pde_operators_->solveState (0);                       CHKERRQ (ierr);
-//
-//     ierr = tumor_->obs_->apply (temp_, tumor_->c_t_);            CHKERRQ (ierr);
-//     ierr = VecAXPY (temp_, -1.0, data);                          CHKERRQ (ierr);
-//     ierr = tumor_->obs_->apply (tumor_->p_t_, temp_);            CHKERRQ (ierr);
-//     ierr = VecScale (tumor_->p_t_, -1.0);                        CHKERRQ (ierr);
-//     ierr = geometricCoupling(
-//       tumor_->work_[7], tumor_->work_[8], tumor_->work_[9], tumor_->work_[10], tumor_->work_[11],
-//       mA1_wm_, mA1_gm_, mA1_csf_, mA1_glm_,  mA1_bg_,
-//       tumor_->c_t_, n_misc_);                                    CHKERRQ (ierr);
-//     // evaluate brain tissue distance meassure || mR - mT ||, mR = mA0(1-c), mT = patient
-//     geometricCouplingAdjoint(&misfit_brain,
-//       xi_wm_, xi_gm_, xi_csf_, xi_glm_,  xi_bg_,
-//       mR_wm_, mR_gm_, mR_csf_, mR_glm_,  mR_bg_,
-//       mT_wm_, mT_gm_, mT_csf_, mT_glm_,  mT_bg_);                CHKERRQ (ierr);
-//     // compute xi * mA0, add    -\xi * mA0 to adjoint final cond.
-//     if(mR_wm_ != nullptr) {
-//   		ierr = VecPointwiseMult (temp_, xi_wm_, mR_wm_);         CHKERRQ (ierr);
-//       ierr = VecAXPY (tumor_->p_t_, -1.0, temp_);                CHKERRQ (ierr);
-//   	}
-//   	if(mR_gm_ != nullptr) {
-//       ierr = VecPointwiseMult (temp_, xi_gm_, mR_gm_);           CHKERRQ (ierr);
-//       ierr = VecAXPY (tumor_->p_t_, -1.0, temp_);                CHKERRQ (ierr);
-//   	}
-//   	if(mR_csf_ != nullptr) {
-//       ierr = VecPointwiseMult (temp_, xi_csf_, mR_csf_);         CHKERRQ (ierr);
-//       ierr = VecAXPY (tumor_->p_t_, -1.0, temp_);                CHKERRQ (ierr);
-//   	}
-//   	if(mR_glm_ != nullptr) {
-//       ierr = VecPointwiseMult (temp_, xi_glm_, mR_glm_);         CHKERRQ (ierr);
-//       ierr = VecAXPY (tumor_->p_t_, -1.0, temp_);                CHKERRQ (ierr);
-//   	}
-//     ierr = VecScale (tumor_->p_t_, 1.0/nc_);                     CHKERRQ (ierr);
-//     // solve adjoint equation with specified final condition
-//     ierr = pde_operators_->solveAdjoint (1);
-//     // evaluate gradient
-//     ierr = tumor_->phi_->applyTranspose (ptemp_, tumor_->p_0_);  CHKERRQ (ierr);
-//     ierr = tumor_->phi_->applyTranspose (dJ, tumor_->c_0_);      CHKERRQ (ierr);
-//     ierr = VecScale (dJ, n_misc_->beta_);                        CHKERRQ (ierr);
-//     ierr = VecAXPY (dJ, -1.0, ptemp_);                           CHKERRQ (ierr);
-//     PetscFunctionReturn(0);
-// }
-//
-
 
 
 /* #### ------------------------------------------------------------------- #### */
