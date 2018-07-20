48dd311de364456df08600b3ac22695f6c310216
Shashank Subramanian <shashank1693@gmail.com>
2018-07-20 13:29:37 -0500

    modified app drivers to read in multiples of data-driven sigma: TODO fix to double

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ### name of results sub-directory
-REGTYPE=L1
+REGTYPE=L2
 ### reg type
-reg_type=L1
+reg_type=L2
 ### name of results sub-directory
-TESTCASE=tc9
+TESTCASE=tc
 
 ### Synthetic data parameters
 rho_data=10
@@ -20,23 +20,24 @@ nt_inv=30
 dt_inv=0.02
 
 ### Interpolation flag
-interp_flag=0
+interp_flag=1
 ### Diffusivity inversion flag
-diffusivity_flag=1
+diffusivity_flag=0
 ### Radial basis flag: 1 - data driven, 0 - grid-based (bounding box)
 basis_type=1
 lam_cont=1
 
 beta=0e-4
-N=128
+N=256
 np=343
-fac=0.5
+fac=2
 space=2.0
 gvf=0.99
 data_thres=0.1
 target_spars=0.99
+dd_fac=4
 
-##space=0.09817477042
+# space=0.09817477042
 
 #declare -a rhoarray=(0)
 #for rho_inv in ${rhoarray[@]}; do
@@ -45,7 +46,7 @@ target_spars=0.99
 # declare -a karray=(0e+00 1e-07 5e-07 1e-06 5e-05 1e-05 5e-05 1e-04 5e-04 1e-03 5e-03 1e-02 5e-02 1e-01 5e-01 1e+00)
 # for k_inv in ${karray[@]}; do
 
-RESULTS_DIR=./results/${REGTYPE}/${TESTCASE}/atlas1
+RESULTS_DIR=./results/${REGTYPE}/
 
 mkdir -p ${RESULTS_DIR}
 
@@ -53,11 +54,11 @@ cat <<EOF > submit
 #!/bin/bash
 
 #SBATCH -J ITP
-#SBATCH -o itp_${TESTCASE}_${REGTYPE}_atlas1
+#SBATCH -o itp_${TESTCASE}_${REGTYPE}
 #SBATCH -p rebels
 #SBATCH -N 1
 #SBATCH -n 20
-#SBATCH -t 24:00:00
+#SBATCH -t 1:00:00
 
 source ~/.bashrc
 export OMP_NUM_THREADS=1
@@ -70,8 +71,10 @@ mpirun build/last/inverse -nx ${N} -ny ${N} -nz ${N} -beta ${beta} \
 -gaussian_volume_fraction ${gvf} \
 -lambda_continuation ${lam_cont} \
 -target_sparsity ${target_spars} \
--threshold_data_driven ${data_thres}
+-threshold_data_driven ${data_thres} \
+-sigma_data_driven ${dd_fac}
 EOF
 
 sbatch submit
 # done
+# -sigma_data_driven ${space}
