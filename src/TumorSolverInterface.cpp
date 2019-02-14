c9e3fcad33d0efb094877086571de578bbe9a142
Shashank Subramanian <shashank1693@gmail.com>
2019-02-13 22:16:37 -0600

    minor smoothing changes

--- src/TumorSolverInterface.cpp
+++ src/TumorSolverInterface.cpp
@@ -165,11 +165,13 @@ PetscErrorCode TumorSolverInterface::solveInverse (Vec prec, Vec d1, Vec d1g) {
     ierr = tumor_->obs_->setDefaultFilter (d1);
     ierr = tumor_->obs_->apply (d1, d1); 
 
-    double *d_ptr;
-    double s_fact = 0.5 * 2.0 * M_PI / n_misc_->n_[0];
-    ierr = VecGetArray (d1, &d_ptr);                                                 CHKERRQ(ierr);
-    ierr = weierstrassSmoother (d_ptr, d_ptr, n_misc_, s_fact);                      CHKERRQ(ierr);  // smooth the data a bit after applying obs
-    ierr = VecRestoreArray (d1, &d_ptr);                                             CHKERRQ(ierr);
+
+    // double *d_ptr;
+    // double s_fact = 0.5 * 2.0 * M_PI / n_misc_->n_[0];
+    // ierr = VecGetArray (d1, &d_ptr);                                                 CHKERRQ(ierr);
+    // ierr = weierstrassSmoother (d_ptr, d_ptr, n_misc_, s_fact);                      CHKERRQ(ierr);  // smooth the data a bit after applying obs
+    // ierr = VecRestoreArray (d1, &d_ptr);                                             CHKERRQ(ierr);
+    
 
     // set target data for inversion (just sets the vector, no deep copy)
     inv_solver_->setData (d1);
@@ -592,11 +594,14 @@ PetscErrorCode TumorSolverInterface::solveInverseCoSaMp (Vec prec, Vec d1, Vec d
     // apply observer on ground truth, store observed data in d
     ierr = tumor_->obs_->apply (d1, d1);  
 
-    double *d_ptr;
-    double s_fact = 0.5 * 2.0 * M_PI / n_misc_->n_[0];
-    ierr = VecGetArray (d1, &d_ptr);                                                 CHKERRQ(ierr);
-    ierr = weierstrassSmoother (d_ptr, d_ptr, n_misc_, s_fact);                      CHKERRQ(ierr);  // smooth the data a bit after applying obs -- half a voxel.
-    ierr = VecRestoreArray (d1, &d_ptr);                                             CHKERRQ(ierr);                                
+
+    // double *d_ptr;
+    // double s_fact = 0.5 * 2.0 * M_PI / n_misc_->n_[0];
+
+    // ierr = VecGetArray (d1, &d_ptr);                                                 CHKERRQ(ierr);
+    // ierr = weierstrassSmoother (d_ptr, d_ptr, n_misc_, s_fact);                      CHKERRQ(ierr);  // smooth the data a bit after applying obs -- half a voxel.
+    // ierr = VecRestoreArray (d1, &d_ptr);                                             CHKERRQ(ierr);                  
+    
 
     // set target data for inversion (just sets the vector, no deep copy)
     inv_solver_->setData (d1);
