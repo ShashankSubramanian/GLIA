8bc7d91ef9364cbbceae41def613c5e3d5f1541d
Shashank Subramanian <shashank1693@gmail.com>
2019-05-15 15:27:57 -0500

    added bounds checking to cuda kernels

--- include/UtilsCuda.h
+++ include/UtilsCuda.h
@@ -18,9 +18,9 @@
 #include <cuda_runtime.h>
 
 #define N_THREADS 512
-#define N_THREADS_X 32
+#define N_THREADS_X 8
 #define N_THREADS_Y 8
-#define N_THREADS_Z 1
+#define N_THREADS_Z 8
 
 // Cuda error checking routines
 
--- src/UtilsCuda.cu
+++ src/UtilsCuda.cu
@@ -15,45 +15,53 @@ __global__ void computeWeierstrassFilter (double *f, double sigma) {
 	int j = threadIdx.y + blockDim.y * blockIdx.y;
 	int k = threadIdx.z + blockDim.z * blockIdx.z;
 
-	double X, Y, Z, Xp, Yp, Zp, twopi;
-	double hx, hy, hz;
-	int64_t ptr;
-	twopi = 2. * CUDART_PI;
-	hx = twopi / n_cuda[0];
-	hy = twopi / n_cuda[1];
-	hz = twopi / n_cuda[2];
+	int64_t ptr = i * isize_cuda[1] * isize_cuda[2] + j * isize_cuda[2] + k;
 
-	X = (istart_cuda[0] + i) * hx;
-	Xp = X - twopi;
-	Y = (istart_cuda[1] + j) * hy;
-	Yp = Y - twopi;
-	Z = (istart_cuda[2] + k) * hz;
-	Zp = Z - twopi;
-	ptr = i * isize_cuda[1] * isize_cuda[2] + j * isize_cuda[2] + k;
-	f[ptr] = exp((-X * X - Y * Y - Z * Z) / sigma / sigma / 2.0)
-			+ exp((-Xp * Xp - Yp * Yp - Zp * Zp) / sigma / sigma / 2.0);
+	if (ptr < isize_cuda[0] * isize_cuda[1] * isize_cuda[2]) {
+		double X, Y, Z, Xp, Yp, Zp, twopi;
+		double hx, hy, hz;
+		
+		twopi = 2. * CUDART_PI;
+		hx = twopi / n_cuda[0];
+		hy = twopi / n_cuda[1];
+		hz = twopi / n_cuda[2];
 
-	f[ptr] += exp((-Xp * Xp - Y * Y - Z * Z) / sigma / sigma / 2.0)
-			+ exp((-X * X - Yp * Yp - Z * Z) / sigma / sigma / 2.0);
+		X = (istart_cuda[0] + i) * hx;
+		Xp = X - twopi;
+		Y = (istart_cuda[1] + j) * hy;
+		Yp = Y - twopi;
+		Z = (istart_cuda[2] + k) * hz;
+		Zp = Z - twopi;
+		
+		f[ptr] = exp((-X * X - Y * Y - Z * Z) / sigma / sigma / 2.0)
+				+ exp((-Xp * Xp - Yp * Yp - Zp * Zp) / sigma / sigma / 2.0);
 
-	f[ptr] += exp((-X * X - Y * Y - Zp * Zp) / sigma / sigma / 2.0)
-			+ exp((-Xp * Xp - Yp * Yp - Z * Z) / sigma / sigma / 2.0);
+		f[ptr] += exp((-Xp * Xp - Y * Y - Z * Z) / sigma / sigma / 2.0)
+				+ exp((-X * X - Yp * Yp - Z * Z) / sigma / sigma / 2.0);
 
-	f[ptr] += exp((-Xp * Xp - Y * Y - Zp * Zp) / sigma / sigma / 2.0)
-			+ exp((-X * X - Yp * Yp - Zp * Zp) / sigma / sigma / 2.0);
+		f[ptr] += exp((-X * X - Y * Y - Zp * Zp) / sigma / sigma / 2.0)
+				+ exp((-Xp * Xp - Yp * Yp - Z * Z) / sigma / sigma / 2.0);
 
-	if (f[ptr] != f[ptr])
-		f[ptr] = 0.; // To avoid Nan
+		f[ptr] += exp((-Xp * Xp - Y * Y - Zp * Zp) / sigma / sigma / 2.0)
+				+ exp((-X * X - Yp * Yp - Zp * Zp) / sigma / sigma / 2.0);
+
+		if (f[ptr] != f[ptr])
+			f[ptr] = 0.; // To avoid Nan
+	}
 }
 
 __global__ void hadamardComplexProduct (cuDoubleComplex *y, double *x) {
 	int i = threadIdx.x + blockDim.x * blockIdx.x;
-	y[i] = cuCmul (y[i], make_cuDoubleComplex(x[i], 0.));
+
+	if (i < osize_cuda[0] * osize_cuda[1] * osize_cuda[2]) 
+		y[i] = cuCmul (y[i], make_cuDoubleComplex(x[i], 0.));
 }
 
 __global__ void hadamardComplexProduct (cuDoubleComplex *y, cuDoubleComplex *x) {
 	int i = threadIdx.x + blockDim.x * blockIdx.x;
-	y[i] = cuCmul (y[i], x[i]);
+
+	if (i < osize_cuda[0] * osize_cuda[1] * osize_cuda[2]) 
+		y[i] = cuCmul (y[i], x[i]);
 }
 
 __global__ void precFactorDiffusion (double *precfactor, double *work) {
@@ -61,49 +69,75 @@ __global__ void precFactorDiffusion (double *precfactor, double *work) {
 	int y = threadIdx.y + blockDim.y * blockIdx.y;
 	int z = threadIdx.z + blockDim.z * blockIdx.z;
 
-	double factor = 1.0 / (n_cuda[0] * n_cuda[1] * n_cuda[2]);
+	int64_t index = x * osize_cuda[1] * osize_cuda[2] + y * osize_cuda[2] + z;
 
-	double X, Y, Z;
-	X = ostart_cuda[0] + x;
-    Y = ostart_cuda[1] + y;
-    Z = ostart_cuda[2] + z;
+	if (index < osize_cuda[0] * osize_cuda[1] * osize_cuda[2]) {
+		double factor = 1.0 / (n_cuda[0] * n_cuda[1] * n_cuda[2]);
 
-    double wx, wy, wz;
-    wx = X;
-    wy = Y;
-    wz = Z;
+		double X, Y, Z;
+		X = ostart_cuda[0] + x;
+	    Y = ostart_cuda[1] + y;
+	    Z = ostart_cuda[2] + z;
 
-    if (X > n_cuda[0] / 2.0)
-        wx -= n_cuda[0];
-    if (X == n_cuda[0] / 2.0)
-        wx = 0;
+	    double wx, wy, wz;
+	    wx = X;
+	    wy = Y;
+	    wz = Z;
 
-    if (Y > n_cuda[1] / 2.0)
-        wy -= n_cuda[1];
-    if (Y == n_cuda[1] / 2.0)
-        wy = 0;
+	    if (X > n_cuda[0] / 2.0)
+	        wx -= n_cuda[0];
+	    if (X == n_cuda[0] / 2.0)
+	        wx = 0;
 
-    if (Z > n_cuda[2] / 2.0)
-        wz -= n_cuda[2];
-    if (Z == n_cuda[2] / 2.0)
-        wz = 0;
+	    if (Y > n_cuda[1] / 2.0)
+	        wy -= n_cuda[1];
+	    if (Y == n_cuda[1] / 2.0)
+	        wy = 0;
 
-    double dt = work[0];
-    double kxx_avg = work[1];
-    double kxy_avg = work[2];
-    double kxz_avg = work[3];
-    double kyz_avg = work[4];
-    double kyy_avg = work[5];
-    double kzz_avg = work[6];
+	    if (Z > n_cuda[2] / 2.0)
+	        wz -= n_cuda[2];
+	    if (Z == n_cuda[2] / 2.0)
+	        wz = 0;
 
-    int64_t index = x * osize_cuda[1] * osize_cuda[2] + y * osize_cuda[2] + z;
-    precfactor[index] = (1. + 0.25 * dt * (kxx_avg * wx * wx + 2.0 * kxy_avg * wx * wy
-                            + 2.0 * kxz_avg * wx * wz + 2.0 * kyz_avg * wy * wz + kyy_avg * wy * wy
-                                            + kzz_avg * wz *wz));
-    if (precfactor[index] == 0)
-        precfactor[index] = factor;
-    else
-        precfactor[index] = factor / precfactor[index];
+	    double dt = work[0];
+	    double kxx_avg = work[1];
+	    double kxy_avg = work[2];
+	    double kxz_avg = work[3];
+	    double kyz_avg = work[4];
+	    double kyy_avg = work[5];
+	    double kzz_avg = work[6];
+
+	    
+	    precfactor[index] = (1. + 0.25 * dt * (kxx_avg * wx * wx + 2.0 * kxy_avg * wx * wy
+	                            + 2.0 * kxz_avg * wx * wz + 2.0 * kyz_avg * wy * wz + kyy_avg * wy * wy
+	                                            + kzz_avg * wz *wz));
+	    if (precfactor[index] == 0)
+	        precfactor[index] = factor;
+	    else
+	        precfactor[index] = factor / precfactor[index];
+	}
+}
+
+__global__ void logisticReactionLinearized (double *c_t_ptr, double *rho_ptr, double *c_ptr, double dt) {
+	int i = threadIdx.x + blockDim.x * blockIdx.x;
+
+	if (i < isize_cuda[0] * isize_cuda[1] * isize_cuda[2]) {
+		double factor = 0., alph = 0.;
+	    factor = exp (rho_ptr[i] * dt);
+	    alph = (c_ptr[i] * factor + 1.0 - c_ptr[i]);
+	    c_t_ptr[i] = c_t_ptr[i] * factor / (alph * alph);
+	}
+}
+
+__global__ void logisticReaction (double *c_t_ptr, double *rho_ptr, double *c_ptr, double dt) {
+	int i = threadIdx.x + blockDim.x * blockIdx.x;
+
+	if (i < isize_cuda[0] * isize_cuda[1] * isize_cuda[2]) {
+		double factor = 0., alph = 0.;
+	    factor = exp (rho_ptr[i] * dt);
+	    alph = (1.0 - c_t_ptr[i]) / c_t_ptr[i];
+	    c_t_ptr[i] = factor / (factor + alph);
+	}
 }
 
 void precFactorDiffusionCuda (double *precfactor, double *work, int *sz) {
@@ -111,7 +145,7 @@ void precFactorDiffusionCuda (double *precfactor, double *work, int *sz) {
 	int n_th_y = N_THREADS_Y;
 	int n_th_z = N_THREADS_Z;
 	dim3 n_threads (n_th_x, n_th_y, n_th_z);
-	dim3 n_blocks (sz[0] / n_th_x, sz[1] / n_th_y, sz[2] / n_th_z);
+	dim3 n_blocks (std::ceil(sz[0] / n_th_x), std::ceil(sz[1] / n_th_y), std::ceil(sz[2] / n_th_z));
 
 	precFactorDiffusion <<< n_blocks, n_threads >>> (precfactor, work);
 
@@ -124,7 +158,7 @@ void computeWeierstrassFilterCuda (double *f, double *sum, double sigma, int *sz
 	int n_th_y = N_THREADS_Y;
 	int n_th_z = N_THREADS_Z;
 	dim3 n_threads (n_th_x, n_th_y, n_th_z);
-	dim3 n_blocks (sz[0] / n_th_x, sz[1] / n_th_y, sz[2] / n_th_z);
+	dim3 n_blocks (std::ceil(sz[0] / n_th_x), std::ceil(sz[1] / n_th_y), std::ceil(sz[2] / n_th_z));
 
 	computeWeierstrassFilter <<< n_blocks, n_threads >>> (f, sigma);
 
@@ -146,7 +180,7 @@ void computeWeierstrassFilterCuda (double *f, double *sum, double sigma, int *sz
 void hadamardComplexProductCuda (cuDoubleComplex *y, double *x, int *sz) {
 	int n_th = N_THREADS;
 
-	hadamardComplexProduct <<< (sz[0] * sz[1] * sz[2]) / n_th, n_th >>> (y, x);
+	hadamardComplexProduct <<< std::ceil((sz[0] * sz[1] * sz[2]) / n_th), n_th >>> (y, x);
 
 	cudaDeviceSynchronize();
 	cudaCheckKernelError ();
@@ -166,33 +200,14 @@ void hadamardComplexProductCuda (cuDoubleComplex *y, cuDoubleComplex *x, int *sz
 	cudaDeviceSynchronize();
 }
 
-__global__ void logisticReactionLinearized (double *c_t_ptr, double *rho_ptr, double *c_ptr, double dt) {
-	int i = threadIdx.x + blockDim.x * blockIdx.x;
-	double factor = 0., alph = 0.;
-
-    factor = exp (rho_ptr[i] * dt);
-    alph = (c_ptr[i] * factor + 1.0 - c_ptr[i]);
-    c_t_ptr[i] = c_t_ptr[i] * factor / (alph * alph);
-    
-}
-
-__global__ void logisticReaction (double *c_t_ptr, double *rho_ptr, double *c_ptr, double dt) {
-	int i = threadIdx.x + blockDim.x * blockIdx.x;
-	double factor = 0., alph = 0.;
-
-    factor = exp (rho_ptr[i] * dt);
-    alph = (1.0 - c_t_ptr[i]) / c_t_ptr[i];
-    c_t_ptr[i] = factor / (factor + alph);
-    
-}
 
 void logisticReactionCuda (double *c_t_ptr, double *rho_ptr, double *c_ptr, double dt, int sz, int linearized) {
 	int n_th = N_THREADS;
 
 	if (linearized == 0)
-		logisticReaction <<< sz / n_th, n_th >>> (c_t_ptr, rho_ptr, c_ptr, dt);
+		logisticReaction <<< std::ceil(sz / n_th), n_th >>> (c_t_ptr, rho_ptr, c_ptr, dt);
 	else
-		logisticReactionLinearized <<< sz / n_th, n_th >>> (c_t_ptr, rho_ptr, c_ptr, dt);
+		logisticReactionLinearized <<< std::ceil(sz / n_th), n_th >>> (c_t_ptr, rho_ptr, c_ptr, dt);
 
 	cudaDeviceSynchronize();
 	cudaCheckKernelError ();
