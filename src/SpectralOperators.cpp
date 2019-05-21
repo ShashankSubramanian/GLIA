d05a9929875b67609818963dbb5ba058d1821127
Shashank Subramanian <shashank1693@gmail.com>
2019-05-20 21:16:12 -0500

    minor bug in cufft type

--- src/SpectralOperators.cpp
+++ src/SpectralOperators.cpp
@@ -14,8 +14,8 @@ void SpectralOperators::setup (int *n, int *isize, int *istart, int *osize, int 
 
         plan_ = accfft_plan_dft_3d_r2c_gpu (n, c_0, (double*) c_hat, c_comm, ACCFFT_MEASURE);
         if (fft_mode_ == CUFFT) {
-            cufft_status = cufftPlan3d (&plan_r2c_, n[0], n[1], n[2], CUFFT_R2C);   cufftCheckError (cufft_status);
-            cufft_status = cufftPlan3d (&plan_c2r_, n[0], n[1], n[2], CUFFT_C2R);   cufftCheckError (cufft_status);
+            cufft_status = cufftPlan3d (&plan_r2c_, n[0], n[1], n[2], CUFFT_D2Z);   cufftCheckError (cufft_status);
+            cufft_status = cufftPlan3d (&plan_c2r_, n[0], n[1], n[2], CUFFT_Z2D);   cufftCheckError (cufft_status);
         }
 
         // define constants for the gpu
