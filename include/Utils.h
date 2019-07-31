aa4c78166234d52d55125279316d1c263d42215d
Shashank Subramanian <shashank1693@gmail.com>
2019-07-31 16:04:45 -0500

    param changes

--- include/Utils.h
+++ include/Utils.h
@@ -334,14 +334,14 @@ class NMisc {
                 // user_cm_[2] = 2 * M_PI / 128 * 76;//52  //X
 
                 // tc1 and 2
-                user_cm_[0] = 2 * M_PI / 128 * 56;//82  //Z
-                user_cm_[1] = 2 * M_PI / 128 * 68;//64  //Y
-                user_cm_[2] = 2 * M_PI / 128 * 72;//52  //X
+                // user_cm_[0] = 2 * M_PI / 128 * 56;//82  //Z
+                // user_cm_[1] = 2 * M_PI / 128 * 68;//64  //Y
+                // user_cm_[2] = 2 * M_PI / 128 * 72;//52  //X
 
                 // casebrats for mass effect
-                // user_cm_[0] = 2 * M_PI / 128 * 72;//82  //Z
-                // user_cm_[1] = 2 * M_PI / 128 * 92;//64  //Y
-                // user_cm_[2] = 2 * M_PI / 128 * 72;//52  //X
+                user_cm_[0] = 2 * M_PI / 128 * 66;//72  //Z
+                user_cm_[1] = 2 * M_PI / 128 * 92;//64  //Y
+                user_cm_[2] = 2 * M_PI / 128 * 72;//52  //X
 
                 // tc2
                 // user_cm_[0] = 2 * M_PI / 128 * 72;//82  //Z
--- scripts/submit.py
+++ scripts/submit.py
@@ -9,7 +9,7 @@ scripts_path = os.path.dirname(os.path.realpath(__file__))
 tumor_dir = scripts_path + '/../'
 params = {}
 params['code_path'] = tumor_dir
-params['results_path'] = tumor_dir + '/results/multispecies/'
+params['results_path'] = tumor_dir + '/results/multispecies_256/'
 params['compute_sys'] = 'rebels'
 
 
