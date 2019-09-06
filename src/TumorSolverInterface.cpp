cf4f2f6422808320786d7de30801c34743f17630
Klaudius Scheufele <kscheufele@austin.utexas.edu>
2019-09-06 11:13:05 -0500

    licence

--- src/TumorSolverInterface.cpp
+++ src/TumorSolverInterface.cpp
@@ -1,21 +1,23 @@
-/*************************************************************************
- *  Copyright (c) 2016-2017.
- *  All rights reserved.
+/**
+ *  SIBIA (Scalable Biophysics-Based Image Analysis)
+ *
+ *  Copyright (C) 2017-2020, The University of Texas at Austin
  *  This file is part of the SIBIA library.
  *
  *  SIBIA is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 2 of the License, or
+ *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  SIBIA is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with SIBIA. If not, see <http://www.gnu.org/licenses/>.
- ************************************************************************/
+ *  along with this program. If not, see the LICENSE file.
+ *
+ **/
 
 #include "TumorSolverInterface.h"
 #include "EventTimings.hpp"
@@ -166,7 +168,7 @@ PetscErrorCode TumorSolverInterface::initialize (
 // ### _____________________________________________________________________ ___
 // ### ///////////////// setParams ///////////////////////////////////////// ###
 PetscErrorCode TumorSolverInterface::setParams (
-    Vec p, 
+    Vec p,
     std::shared_ptr<TumorSettings> tumor_params = {})
 {
     PetscFunctionBegin;
