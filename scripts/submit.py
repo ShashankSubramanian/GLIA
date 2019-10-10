7bc2563b2c31f7793b2dbc7d9da2948740765485
Shashank Subramanian <shashank1693@gmail.com>
2019-10-10 13:02:41 -0500

    params++

--- scripts/submit.py
+++ scripts/submit.py
@@ -9,7 +9,7 @@ scripts_path = os.path.dirname(os.path.realpath(__file__))
 tumor_dir = scripts_path + '/../'
 params = {}
 params['code_path'] = tumor_dir
-params['results_path'] = tumor_dir + '/results/check-tc1/'
+params['results_path'] = tumor_dir + '/results/jakob-low/'
 params['compute_sys'] = 'maverick2'
 
 
@@ -17,12 +17,14 @@ params['compute_sys'] = 'maverick2'
 # ### Real data
 # params['data_path'] = tumor_dir + '/results/tc2_128/data.nc'
 # # # ## Atlas
-# params['gm_path'] = '/work/05027/shas1693/maverick2/pglistr_tumor/brain_data/sri_atlas/sri_gm_256x256x256_aff2jakob.nc'
-# params['wm_path'] = '/work/05027/shas1693/maverick2/pglistr_tumor/brain_data/sri_atlas/sri_wm_256x256x256_aff2jakob.nc'
-# params['csf_path'] = '/work/05027/shas1693/maverick2/pglistr_tumor/brain_data/sri_atlas/sri_csf_256x256x256_aff2jakob.nc'
-# params['gm_path'] = '/workspace/shashank/label_maps/tcia_09_141/gm.nc'
-# params['wm_path'] = '/workspace/shashank/label_maps/tcia_09_141/wm.nc'
-# params['csf_path'] = '/workspace/shashank/label_maps/tcia_09_141/csf.nc'
+#params['gm_path'] = '/work/05027/shas1693/maverick2/pglistr_tumor/brain_data/sri_atlas/sri_gm_256x256x256_aff2jakob.nc'
+#params['wm_path'] = '/work/05027/shas1693/maverick2/pglistr_tumor/brain_data/sri_atlas/sri_wm_256x256x256_aff2jakob.nc'
+#params['csf_path'] = '/work/05027/shas1693/maverick2/pglistr_tumor/brain_data/sri_atlas/sri_csf_256x256x256_aff2jakob.nc'
+
+params['gm_path'] = tumor_dir + "/brain_data/jakob/jakob_gm.nc" 
+params['wm_path'] = tumor_dir + "/brain_data/jakob/jakob_wm.nc" 
+params['csf_path'] = tumor_dir + "/brain_data/jakob/jakob_csf.nc" 
+
 
 if params['compute_sys'] == 'rebels':
     queue = 'rebels'
