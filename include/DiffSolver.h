d672f88a311d2d8d9b94537f15c21839ab8609f4
Shashank Subramanian <shashank1693@gmail.com>
2020-04-08 11:40:28 -0500

    DiffSolver: n_misc --> params

--- include/DiffSolver.h
+++ include/DiffSolver.h
@@ -1,50 +1,46 @@
+#endif
 #ifndef DIFFSOLVER_H_
 #define DIFFSOLVER_H_
 
-#include "Utils.h"
 #include "DiffCoef.h"
+#include "Parameters.h"
 #include "SpectralOperators.h"
 
 struct Ctx {
-	std::shared_ptr<DiffCoef> k_;
-	std::shared_ptr<NMisc> n_misc_;
-	std::shared_ptr<SpectralOperators> spec_ops_;
-	fft_plan *plan_;
-	ScalarType dt_;
-	Vec temp_;
-	ScalarType *precfactor_;
-	ScalarType *work_cuda_;
+  std::shared_ptr<DiffCoef> k_;
+  std::shared_ptr<Parameters> params_;
+  std::shared_ptr<SpectralOperators> spec_ops_;
+  fft_plan *plan_;
+  ScalarType dt_;
+  Vec temp_;
+  ScalarType *precfactor_;
+  ScalarType *work_cuda_;
 
-	~Ctx () {}
+  ~Ctx() {}
 };
 
 class DiffSolver {
-	public:
-		DiffSolver (std::shared_ptr<NMisc> n_misc, std::shared_ptr<SpectralOperators> spec_ops, std::shared_ptr<DiffCoef> k);
+ public:
+  DiffSolver(std::shared_ptr<Parameters> params, std::shared_ptr<SpectralOperators> spec_ops, std::shared_ptr<DiffCoef> k);
 
-		KSP ksp_;
-		Mat A_;
+  KSP ksp_;
+  Mat A_;
+  PC pc_;
+  Vec rhs_;
+  int ksp_itr_;
+  std::shared_ptr<Ctx> ctx_;
 
-		PC pc_;
+  PetscErrorCode solve(Vec c, ScalarType dt);
+  PetscErrorCode precFactor();
 
-		Vec rhs_;
-
-		int ksp_itr_;
-
-		std::shared_ptr<Ctx> ctx_;
-
-		PetscErrorCode solve (Vec c, ScalarType dt);
-		PetscErrorCode precFactor ();
-
-		virtual ~DiffSolver ();
-
+  virtual ~DiffSolver();
 };
 
-//Helper functions for KSP solve
-PetscErrorCode operatorA (Mat A, Vec x, Vec y);
-PetscErrorCode operatorCreateVecs (Mat A, Vec *left, Vec *right);
-PetscErrorCode precFactor (ScalarType *precfactor, std::shared_ptr<Ctx> ctx);
-PetscErrorCode applyPC (PC pc, Vec x, Vec y);
-PetscErrorCode diffSolverKSPMonitor (KSP ksp, PetscInt its, PetscReal rnorm, void *ptr);
+// Helper functions for KSP solve
+PetscErrorCode operatorA(Mat A, Vec x, Vec y);
+PetscErrorCode operatorCreateVecs(Mat A, Vec *left, Vec *right);
+PetscErrorCode precFactor(ScalarType *precfactor, std::shared_ptr<Ctx> ctx);
+PetscErrorCode applyPC(PC pc, Vec x, Vec y);
+PetscErrorCode diffSolverKSPMonitor(KSP ksp, PetscInt its, PetscReal rnorm, void *ptr);
 
 #endif
