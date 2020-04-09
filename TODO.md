bb21d19dd3ab18e7d4341e07835fd3bcc67f5470
Shashank Subramanian <shashank1693@gmail.com>
2020-04-08 22:32:19 -0500

    Phi: n_misc --> params; removed serial macro

--- TODO.md
+++ TODO.md
@@ -75,6 +75,11 @@ No, bg is needed for lame parameters in the bg (which is a hard stiff material).
 - (S) n_misc --> params changes: [done]
 
 --- 
+### Phi
+- (S) n_misc --> params changes: [done]
+- (S) remove SERIAL macro: [done]
+
+--- 
 ### DiffCoef, ReacCoef
 - (S) n_misc --> params changes: [done]
 - (S) Move sinusoidal setvalues to TestSuite.cpp: [done]
--- include/Phi.h
+++ include/Phi.h
@@ -1,14 +1,11 @@
 #ifndef PHI_H_
 #define PHI_H_
 
-#include "Utils.h"
 #include "MatProp.h"
-#include <mpi.h>
-#include <omp.h>
 
 class Phi {
 	public:
-		Phi (std::shared_ptr<NMisc> n_misc,  std::shared_ptr<SpectralOperators> spec_ops);
+		Phi (std::shared_ptr<Parameters> params,  std::shared_ptr<SpectralOperators> spec_ops);
 
 		std::vector<Vec> phi_vec_;
 
@@ -26,19 +23,19 @@ class Phi {
 		bool compute_;
 
 		std::shared_ptr<MatProp> mat_prop_;
-		std::shared_ptr<NMisc> n_misc_;
+		std::shared_ptr<Parameters> params_;
 		std::shared_ptr<SpectralOperators> spec_ops_;
 		std::vector<ScalarType> centers_;   //Vector of centers for the gaussians
 		std::vector<ScalarType> centers_temp_; // Keeps track of centers if phis ever need to be changed
 
-		PetscErrorCode setGaussians (std::array<ScalarType, 3>& user_cm, ScalarType sigma, ScalarType spacing_factor, int np); //, std::shared_ptr<NMisc> n_misc);   //Bounding box
+		PetscErrorCode setGaussians (std::array<ScalarType, 3>& user_cm, ScalarType sigma, ScalarType spacing_factor, int np); //, std::shared_ptr<Parameters> params);   //Bounding box
 		PetscErrorCode setGaussians (Vec data);		
 		PetscErrorCode setGaussians (std::string file, bool read_comp_data = false);
     	PetscErrorCode setLabels (Vec labels) {labels_ = labels; PetscFunctionReturn(0);}																						//Adaptive phis
 		PetscErrorCode setValues (std::shared_ptr<MatProp> mat_prop);
 		PetscErrorCode phiMesh (ScalarType *center);
-		PetscErrorCode initialize (ScalarType *out, std::shared_ptr<NMisc> n_misc, ScalarType *center);
-		PetscErrorCode truncate (ScalarType *out, std::shared_ptr<NMisc> n_misc, ScalarType *center);
+		PetscErrorCode initialize (ScalarType *out, std::shared_ptr<Parameters> params, ScalarType *center);
+		PetscErrorCode truncate (ScalarType *out, std::shared_ptr<Parameters> params, ScalarType *center);
 		PetscErrorCode apply (Vec out, Vec p);
 		PetscErrorCode applyTranspose (Vec pout, Vec in);
 		void modifyCenters (std::vector<int> support_idx);    	// Picks only the basis needed in the restricted subspace
